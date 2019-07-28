f62cb31d669a3d1447f9eba6240607c4658e69a2
lianxianzhong <1805656188@qq.com>
2019-07-28 19:32:39 +0800

    Create precache-manifest.d38bcad97a7bebadedcc2946bb7f7d86.js

---
+++ qumalvxing/dist/precache-manifest.d38bcad97a7bebadedcc2946bb7f7d86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6fc6f381fffd5015ba1f",
+    "url": "/js/chunk-vendors.36072df8.js"
+  },
+  {
+    "revision": "d0deecb77f96f6129152",
+    "url": "/js/app.be1716f0.js"
+  },
+  {
+    "revision": "8f0e0d1a6020665f9ecef15513b887af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7629b90a2783229756ca2964c002108",
+    "url": "/img/iconfont.a7629b90.svg"
+  },
+  {
+    "revision": "6fc6f381fffd5015ba1f",
+    "url": "/css/chunk-vendors.23d90689.css"
+  },
+  {
+    "revision": "d0deecb77f96f6129152",
+    "url": "/css/app.0e69b68e.css"
+  }
+];
