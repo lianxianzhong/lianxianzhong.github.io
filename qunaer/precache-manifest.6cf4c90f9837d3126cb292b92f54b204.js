bf6efe63462cc3369145f85ded5c618e2adff7aa
lianxianzhong <1805656188@qq.com>
2019-08-09 15:56:44 +0800

    '去哪网'

---
+++ qunaer/precache-manifest.6cf4c90f9837d3126cb292b92f54b204.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6fc6f381fffd5015ba1f",
+    "url": "/js/chunk-vendors.36072df8.js"
+  },
+  {
+    "revision": "29b0d541eb42dd95395f",
+    "url": "/js/app.d080b980.js"
+  },
+  {
+    "revision": "0bf745f67e3fc1bc67c9fd1591a11bf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7629b90a2783229756ca2964c002108",
+    "url": "/img/iconfont.a7629b90.svg"
+  },
+  {
+    "revision": "6fc6f381fffd5015ba1f",
+    "url": "/css/chunk-vendors.23d90689.css"
+  },
+  {
+    "revision": "29b0d541eb42dd95395f",
+    "url": "/css/app.96b64228.css"
+  }
+];
